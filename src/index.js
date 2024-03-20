7dda7cd5a31ffbf6a5e80e356ae922a2c77ced28
Ravindranath Singh <ravindranathsingh391@gmail.com>
2024-03-20 20:11:15 +0530

    Added tweet model and repo

--- src/index.js
+++ src/index.js
@@ -3,10 +3,18 @@ const express = require('express')
 const app = express();
 
 const { PORT } = require('./config/serverConfig')
+const connect = require('./config/database')
+const TweetRepository = require('./repository/tweet-repo')
+const Comment = require('./models/comment')
 
 const serverSetUp = () => {
-    app.listen(PORT, () => {
+    app.listen(PORT, async () => {
         console.log(`Server Started at Port ${PORT}`)
+        await connect();
+        console.log('MongoDB Connected')
+        const tweetRepo = new TweetRepository();
+        const tweet = await tweetRepo.getWithComments('65faf31a47bc7f5e48680d0e')
+        console.log(tweet)
     })
 }
 
