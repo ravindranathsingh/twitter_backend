4f8c8cb121b77be3a6cc692022888b47af68bdb4
Ravindranath Singh <ravindranathsingh391@gmail.com>
2024-03-21 22:18:57 +0530

    Added controller and router for tweet create api

--- src/index.js
+++ src/index.js
@@ -1,18 +1,22 @@
 import express from 'express'
+import bodyParser from 'body-parser'
 import { connect } from './config/database.js'
 import { PORT } from './config/serverConfig.js'
+import apiRoutes from './routes/index.js';
+
 const app = express();
 
-import service from './services/tweet-service.js'
-
 const serverSetUp = () => {
+
+    app.use(bodyParser.json())
+    app.use(bodyParser.urlencoded({extended: true}))
+
+    app.use('/api', apiRoutes)
+    
     app.listen(PORT, async () => {
         console.log(`Server Started at Port ${PORT}`)
         await connect();
         console.log('MongoDB Connected')
-        let ser = new service();
-        const res = await ser.create({content: 'new Tweet by #RAVI'})
-        console.log(res)
     })
 }
 
