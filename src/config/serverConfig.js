55ac2941ad62d5299e0a547c367d7961df32b01a
Ravindranath Singh <ravindranathsingh391@gmail.com>
2024-03-23 01:39:08 +0530

    Added S3 config and multer object

--- src/config/serverConfig.js
+++ src/config/serverConfig.js
@@ -6,5 +6,17 @@ dotenv.config();
 const PORT = process.env.PORT;
 const SALT = bcrypt.genSaltSync(9);
 const KEY = process.env.KEY
+const AWS_REGION = process.env.AWS_REGION
+const AWS_SECRET_ACCESS_KEY = process.env.AWS_SECRET_ACCESS_KEY
+const ACCESS_KEY_ID = process.env.ACCESS_KEY_ID
+const BUCKET_NAME = process.env.BUCKET_NAME
 
-export { PORT, SALT, KEY };
+export {
+    PORT,
+    SALT,
+    KEY,
+    AWS_REGION,
+    AWS_SECRET_ACCESS_KEY,
+    ACCESS_KEY_ID,
+    BUCKET_NAME
+};
